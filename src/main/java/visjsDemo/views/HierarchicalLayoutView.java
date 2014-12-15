cc870faff1c17c56b1330c5164a9c71af5cf115c
sameera roshan <sameeraroshanuom@gmail.com>
2014-12-15 17:16:55 +0530

    added hierarchical layout view

---
+++ src/main/java/visjsDemo/views/HierarchicalLayoutView.java
@@ -0,0 +1,17 @@
+package visjsDemo.views;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.HorizontalLayout;
+import sun.awt.HorizBagLayout;
+
+/**
+ * Created by roshans on 12/15/2014.
+ */
+public class HierarchicalLayoutView extends HorizontalLayout implements View{
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent viewChangeEvent) {
+
+    }
+}
